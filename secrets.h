afbcb1efc46302e6f8e1401844ca39a71ab70ad6
Max Kosnar <tevakosnar@gmail.com>
2023-07-29 11:21:40 +0200

    Adding empty secrets.h

---
+++ secrets.h
@@ -0,0 +1,12 @@
+//
+// Created by Max Kosnar on 24/07/2023.
+//
+
+#include <string>
+
+#ifndef QTMMAUD_SECRETS_H
+#define QTMMAUD_SECRETS_H
+
+//Put you Google Api Key Here
+std::string GoogleApiKey = "";
+#endif //QTMMAUD_SECRETS_H
